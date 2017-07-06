190d6669746e0ab7b244aa2c425fac3c008b8759
tilla1145 <mmsaidov@gmail.com>
2017-07-06 12:10:01 +0600

    Update getRequests.js

--- controllers/getRequests.js
+++ controllers/getRequests.js
@@ -1,2 +1,12 @@
 var app = require('express')()
 var connection = require('./connection/pool')
+
+module.exports = function(app)
+{
+  var getAllCategories = 'select distinct name as category from categories'
+  app.get('/getAllCategories', function(request, response)
+  {
+    connection.query()
+  })
+
+}
